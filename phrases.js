a96d5c9e17bfd5c0a2180f54f10d5139a1594bb5
anvin23 <andrey1vinnik@gmail.com>
2020-09-25 12:30:32 +0300

    Add bot answer phrases

---
+++ phrases.js
@@ -0,0 +1,7 @@
+const phrases = ["Let's talk more about ", "What else you like to do  ",
+    "Tell me more about ", "What do you think about ", "What else you can tell me ",
+    "I also like ", "Are you a good specialist in ", "Can you rephrase your question?",  
+    "I think that is a good idea to have a ", "Maybe you can tell me more about ", 
+    "How often you take a ", "What's happen?", "Are you all right?", 
+    "What you should do with ", "Do you have a ", "Do you ask a specialist? ", 
+    "Do you tell somebody about "]
