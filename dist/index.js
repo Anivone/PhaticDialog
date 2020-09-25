f721634b867965004761c02df0d629227bf09fa7
Maksym Voloshko <anivone18052001@gmail.com>
2020-09-25 12:45:48 +0300

    test commit 1

--- dist/index.js
+++ dist/index.js
@@ -4,16 +4,51 @@ const wordpos = (window.wordpos = new WordPOS({
   debug: true,
 }));
 
-$('#message').on('submit', event => {
-    console.log('submitted');
-    wordpos.getPOS($('#message-text').val(), result => {
-        console.log(result);
-        let nouns = function() {
-            return result.nouns.filter(x => result.adjectives.indexOf(x) === -1);
-        }();
-        $('.replies').append(
-            `<p>${nouns}</p>`
-        )
-    });
-    return false;
-})
+let phraseCounter = {};
+
+const phrases = [
+  "Tell me more about ",
+  "What can you tell me about ",
+  "Please, don`t be so shortspoken",
+];
+
+for (const phrase of phrases) {
+  phraseCounter[phrase] = 0;
+}
+
+let userMsg = function (msg) {
+  return `<div class="user-message">
+            <p class="msg">${msg}</p>
+          </div>`;
+};
+
+let botMsg = function (msg) {
+  return `<div class="bot-message">
+            <p class="msg">${msg}</p>
+          </div>`;
+};
+
+let getRandomElem = function (array) {
+  let index = Math.floor(Math.random() * array.length);
+  return array[index];
+};
+
+let processReply = function (result) {
+  let selectedNoun = result.nouns[result.nouns.length - 1];
+
+  return selectedNoun;
+};
+
+$("#message").on("submit", (event) => {
+  console.log("submitted");
+
+  $(".replies").append(userMsg($("#message-text").val()));
+
+  wordpos.getPOS($("#message-text").val(), (result) => {
+    console.log(result);
+    $(".replies").append(botMsg(processReply(result)));
+  });
+
+  $("#message-text").val("");
+  return false;
+});
